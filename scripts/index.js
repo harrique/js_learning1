3141dd23e0eba31045adfc6043e6e90f370a5c2f
harrique <harrique@gmial.com>
2018-11-25 01:10:26 +0300

    3 add function in module

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,16 @@
 import '../styles/index.scss';
+import cdisplay from "./modules/display.js";
 
 console.log('OK');
+
+let a = {
+  b: 5,
+  c: 10,
+};
+
+let d = [1,2,"333"];
+let e = function(x){
+    console.log(x);
+};
+
+cdisplay("Hello world" , a, d, e);
