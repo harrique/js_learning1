13d37e41e6a3e60fbd8f9d1a5f980c88402c30b4
harrique <harrique@gmial.com>
2018-11-25 20:23:13 +0300

    3.1 Prototype (Array example)

--- scripts/index.js
+++ scripts/index.js
@@ -3,30 +3,16 @@
 import '../styles/index.scss';
 import display from "./modules/display.js";
 
-let cat = {
-    name: {first: 'Vasya', last: 'The best'},
-    age: 3,
-    color: 'gray'
-};
+let arr1 = ['red', 'blue', 'green'];
+let arr2 = ['black', 'white', 'yellow', 'pink'];
 
-Object.defineProperty(cat, 'fullName', {
-   get: function () {
-       return this.name.first + " " + this.name.last;
-   },
-   set: function (value) {
-       let nameParts = value.split(' ');
-       this.name.first = nameParts[0];
-       this.name.last = nameParts[1];
-   } 
-});
+Object.defineProperty(Array.prototype, 'last',
+    {
+        get: function(){
+            return this[this.length - 1];
+        }
+    }
+);
 
-display(cat.fullName);
-
-cat.fullName = "John Doe";
-
-display(cat.fullName);
-
-/* result:
-Vasya The best
-John Doe
-*/
+display(arr1.last);
+display(arr2.last);
