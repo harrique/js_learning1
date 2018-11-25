57b0bdad151c67eecd1a038e793a638707d8c322
harrique <harrique@gmial.com>
2018-11-25 20:53:18 +0300

    3.2 Prototype (prototype vs instance properties)

--- scripts/index.js
+++ scripts/index.js
@@ -3,16 +3,46 @@
 import '../styles/index.scss';
 import display from "./modules/display.js";
 
-let arr1 = ['red', 'blue', 'green'];
-let arr2 = ['black', 'white', 'yellow', 'pink'];
+function Cat(name, color){
+    this.name = name;
+    this.color = color;
+}
 
-Object.defineProperty(Array.prototype, 'last',
-    {
-        get: function(){
-            return this[this.length - 1];
-        }
-    }
-);
+let fluffy = new Cat('Fluffy', 'white');
+let muffin = new Cat('Muffin', 'brown');
 
-display(arr1.last);
-display(arr2.last);
+Cat.prototype.age = 3;
+
+display(fluffy.age, muffin.__proto__.age);
+/* result:
+3
+3
+*/
+
+display(Cat.prototype === muffin.__proto__); // true
+
+fluffy.age = 5;
+display(fluffy.age, fluffy.__proto__.age); // 5   3
+
+display(Object.keys(fluffy), Object.keys(muffin));
+/* result:
+Array {
+    0: name
+    1: color
+    2: age
+}
+Array {
+    0: name
+    1: color
+}
+*/
+display(fluffy.hasOwnProperty('age'), muffin.hasOwnProperty('age'));
+display(JSON.stringify(fluffy), JSON.stringify(muffin));
+/*result:
+{"name":"Fluffy","color":"white","age":5}
+{"name":"Muffin","color":"brown"}
+*/
+Cat.prototype.age = 4;
+display(fluffy.age, muffin.age); // 5   4
+
+
