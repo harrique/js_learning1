7aabfb14ebf668fa805339d432a09eccf8fb76e4
harrique <harrique@gmial.com>
2018-11-25 21:04:52 +0300

    3.3 Prototype (changing function prototype)

--- scripts/index.js
+++ scripts/index.js
@@ -8,41 +8,17 @@ function Cat(name, color){
     this.color = color;
 }
 
+Cat.prototype.age = 4;
+
 let fluffy = new Cat('Fluffy', 'white');
 let muffin = new Cat('Muffin', 'brown');
 
-Cat.prototype.age = 3;
+display(fluffy.age, muffin.age);    // 4   4
+display(Cat.prototype === fluffy.__proto__);    // true
 
-display(fluffy.age, muffin.__proto__.age);
-/* result:
-3
-3
-*/
+Cat.prototype = {age: 6};
 
-display(Cat.prototype === muffin.__proto__); // true
+let snowbell = new Cat('Snowbell', 'black');
+display(fluffy.age, snowbell.age);    // 4   6
 
-fluffy.age = 5;
-display(fluffy.age, fluffy.__proto__.age); // 5   3
-
-display(Object.keys(fluffy), Object.keys(muffin));
-/* result:
-Array {
-    0: name
-    1: color
-    2: age
-}
-Array {
-    0: name
-    1: color
-}
-*/
-display(fluffy.hasOwnProperty('age'), muffin.hasOwnProperty('age'));
-display(JSON.stringify(fluffy), JSON.stringify(muffin));
-/*result:
-{"name":"Fluffy","color":"white","age":5}
-{"name":"Muffin","color":"brown"}
-*/
-Cat.prototype.age = 4;
-display(fluffy.age, muffin.age); // 5   4
-
-
+display(Cat.prototype === fluffy.__proto__);    // false
