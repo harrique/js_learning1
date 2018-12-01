0c77db585f0bed16b9b022c38e86c2d4653339d4
harrique <harrique@gmial.com>
2018-12-02 02:05:41 +0300

    DP.4.0 Install Angular

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,6 @@
 "use strict";
 
 import '../styles/index.scss';
-import display from "./modules/display.js";
 
-class Animal{
-    constructor(voice){
-        this.voice = voice || 'grunt';
-    }
-
-    speak(){
-        display(this.voice);
-    }
-}
-
-class Cat extends Animal{
-    constructor(name, color){
-        super('Meow');
-        this.name = name;
-        this.color = color;
-    }
-}
-
-let fluffy = new Cat('Fluffy', 'white');
-fluffy.speak(); //Meow
-
-display(Object.keys(fluffy.__proto__.__proto__));
+let angular = require('angular');
+let ngModul = angular.module('app', []);
