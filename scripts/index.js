cfe3f63dc41c7334d8520fe458647e43355fe641
harrique <harrique@gmial.com>
2018-12-02 02:34:55 +0300

    DP.4.1 Angular - simple constructor 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,24 @@
 "use strict";
 
 import '../styles/index.scss';
+import angular from "angular";
 
-let angular = require('angular');
-let ngModul = angular.module('app', []);
+let ngModule = angular.module('taskManager', []);
+
+let taskController = function(){
+    let ctrl = this;
+    ctrl.tasks = [{name: 'task1'},
+        {name: 'task 2', completed: true}];
+
+    this.complete = function (item) {
+        if (!ctrl.tasks[item].completed) {
+            console.log('completed task ' + ctrl.tasks[item].name);
+            ctrl.tasks[item].completed = true;
+        }
+        else{
+            ctrl.tasks[item].completed = false;
+        }
+    };
+};
+
+ngModule.controller('taskCtrl', taskController);
